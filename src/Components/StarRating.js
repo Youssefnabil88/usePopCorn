33b3b4e27d3f74f89b54f857c654494ae47d26bf
Youssefnabil88 <youssefwaer145@gmail.com>
2024-09-10 23:50:57 +0300

    Improve reusability of the StarRating component

--- src/Components/StarRating.js
+++ src/Components/StarRating.js
@@ -1,4 +1,3 @@
-import { keyboard } from "@testing-library/user-event/dist/keyboard";
 import { useState } from "react";
 
 const containerStyle = {
@@ -9,6 +8,7 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+  color: "red",
 };
 
 const textStyle = {
@@ -16,47 +16,68 @@ const textStyle = {
   margin: "0",
 };
 
-const starStyle = {
-  display: "block",
-  width: "48px",
-  height: "48px",
-  cursor: "pointer",
-};
-
-export default function StarRating({ numberOfStars = 5 }) {
+export default function StarRating({
+  numberOfStars = 5,
+  size = 48,
+  color = "#fcc419",
+  arr = [],
+}) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
-
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {/* Instead to write one element per star */}
+
         {Array.from({ length: numberOfStars }, (_, i) => (
           <Star
             key={i}
             setRating={() => setRating(i + 1)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            handleTempRatingEnter={() => setTempRating(i+1)}
+            handleTempRatingEnter={() => setTempRating(i + 1)}
             tempRating={tempRating}
-            handleTempRatingLeave={()=> setTempRating(0)}
+            handleTempRatingLeave={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{  tempRating || rating  || ""}</p>
+      <p style={textStyle}>{tempRating?  arr[tempRating -1 ]: arr.length === numberOfStars ? arr[rating-1]:tempRating || rating || ""}</p>
     </div>
   );
 }
 
-function Star({ setRating, full, handleTempRatingEnter,tempRating,handleTempRatingLeave }) {
+function Star({
+  setRating,
+  full,
+  handleTempRatingEnter,
+  handleTempRatingLeave,
+  color,
+  size,
+  word,
+}) {
+  const starStyle = {
+    display: "block",
+    width: `${size}px`,
+    height: `${size}px`,
+    cursor: "pointer",
+  };
+
   return (
-    <span role="button" style={starStyle} onClick={setRating} onMouseEnter={handleTempRatingEnter} onMouseLeave={handleTempRatingLeave}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={setRating}
+      onMouseEnter={handleTempRatingEnter}
+      onMouseLeave={handleTempRatingLeave}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -65,7 +86,7 @@ function Star({ setRating, full, handleTempRatingEnter,tempRating,handleTempRati
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
@@ -78,4 +99,3 @@ function Star({ setRating, full, handleTempRatingEnter,tempRating,handleTempRati
     </span>
   );
 }
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import StarRating from './Components/StarRating';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./Components/StarRating";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating />
+    <StarRating color="red" size={29} />
+    <StarRating color="red" size={29} />
+    <StarRating arr={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
   </React.StrictMode>
 );
-
